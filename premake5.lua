bf11ad9a16e99466ff417dcea0f1b908e44180c3
UndeadKudos <jenkins01tj@gmail.com>
2023-01-29 21:36:30 +0000

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,140 +1,141 @@
 workspace "Crisp"
 	architecture "x64"
+	startproject "Game"
 	
 	-- Configuration Settings --
-	
+		
 	configurations {
 		"Debug",
 		"Release",
 		"Distribution"	-- Distribution is alike release but more heavily stripped --
 	}
-	
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"	
-
-include "premake5-SDL.lua"
-include "premake5-SDLmain.lua"
-include "premake5-GLAD.lua"
-include "premake5-imgui.lua"
-
-project "Crisp"
-	location "Crisp"
-	kind "SharedLib"
-	language "C++"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("build/" .. outputdir .. "/%{prj.name}")
-	
-	pchheader "CrispPCH.h"
-	-- PCH source is needed for vs studio, will be ingnored by other platforms --
-	pchsource "Crisp/src/CrispPCH.cpp"
-
-	files {
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.hpp",
-		"%{prj.name}/src/**.c",
-		"%{prj.name}/src/**.cpp"
-	}
-	
-	includedirs {
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{prj.name}/vendor/SDL2/include",
-		"%{prj.name}/vendor/GLAD/include",
-		"%{prj.name}/vendor/imgui",
-	}
-
-	links {
-		"SDL",
-		"SDLmain",
-		"GLAD",
-		"imgui"
-	}
-	
-	-- Window Build Settings --
-	
-	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
 		
-		defines {
-			"CRISP_PLATFORM_WINDOWS",
-			"CRISP_BUILD_DLL",
-			"IMGUI_IMPL_OPENGL_LOADER_CUSTOM"
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"	
+	
+	include "premake5-SDL.lua"
+	include "premake5-SDLmain.lua"
+	include "premake5-GLAD.lua"
+	include "premake5-imgui.lua"
+	
+	project "Crisp"
+		location "Crisp"
+		kind "SharedLib"
+		language "C++"
+		staticruntime "Off"
+		
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("build/" .. outputdir .. "/%{prj.name}")
+		
+		pchheader "CrispPCH.h"
+		-- PCH source is needed for vs studio, will be ingnored by other platforms --
+		pchsource "Crisp/src/CrispPCH.cpp"
+	
+		files {
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.hpp",
+			"%{prj.name}/src/**.c",
+			"%{prj.name}/src/**.cpp"
 		}
 		
-		postbuildcommands {
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Game")
+		includedirs {
+			"%{prj.name}/src",
+			"%{prj.name}/vendor/spdlog/include",
+			"%{prj.name}/vendor/SDL2/include",
+			"%{prj.name}/vendor/GLAD/include",
+			"%{prj.name}/vendor/imgui",
+		}
+	
+		links {
+			"SDL",
+			"SDLmain",
+			"GLAD",
+			"imgui"
 		}
 		
-	-- Configuration Settings --
+		-- Window Build Settings --
 		
-	filter "configurations:Debug"
-		defines "CRISP_DEBUG"
-		buildoptions "/MDd"
-		symbols "On"
+		filter "system:windows"
+			cppdialect "C++20"
+			systemversion "latest"
+			
+			defines {
+				"CRISP_PLATFORM_WINDOWS",
+				"CRISP_BUILD_DLL",
+				"IMGUI_IMPL_OPENGL_LOADER_CUSTOM"
+			}
+			
+			postbuildcommands {
+				("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Game/\"")
+			}
+			
+		-- Configuration Settings --
+			
+		filter "configurations:Debug"
+			defines "CRISP_DEBUG"
+			runtime "Debug"
+			symbols "On"
+			
+		filter "configurations:Release"
+			defines "CRISP_RELEASE"
+			runtime "Release"
+			optimize "On"
+			
+		filter "configurations:Distribution"
+			defines "CRISP_DISTRIBUTION"
+			runtime "Release"
+			optimize "On"
+	
+	project "Game"
+		location "Game"
+		kind "ConsoleApp"
+		language "C++"
+		staticruntime "Off"
 		
-	filter "configurations:Release"
-		defines "CRISP_RELEASE"
-		buildoptions "/MD"
-		optimize "On"
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("build/" .. outputdir .. "/%{prj.name}")
 		
-	filter "configurations:Distribution"
-		defines "CRISP_DISTRIBUTION"
-		buildoptions "/MD"
-		optimize "On"
-
-project "Game"
-	location "Game"
-	kind "ConsoleApp"
-	language "C++"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("build/" .. outputdir .. "/%{prj.name}")
-	
-	files {
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.hpp",
-		"%{prj.name}/src/**.c",
-		"%{prj.name}/src/**.cpp"
-	}
-	
-	includedirs {
-		"Crisp/vendor/spdlog/include",
-		"Crisp/vendor/SDL2/include",
-		"Crisp/src"
-	}
-	
-	links {
-		"Crisp",
-		"SDL",
-		"SDLmain"
-	}
-	
-	-- Window Build Settings --
-	
-	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
-		
-		defines {
-			"CRISP_PLATFORM_WINDOWS"
+		files {
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.hpp",
+			"%{prj.name}/src/**.c",
+			"%{prj.name}/src/**.cpp"
 		}
 		
-	-- Configuration Settings --
+		includedirs {
+			"Crisp/vendor/spdlog/include",
+			"Crisp/vendor/SDL2/include",
+			"Crisp/src"
+		}
 		
-	filter "configurations:Debug"
-		defines "CRISP_DEBUG"
-		buildoptions "/MDd"
-		symbols "On"
+		links {
+			"Crisp",
+			"SDL",
+			"SDLmain"
+		}
 		
-	filter "configurations:Release"
-		defines "CRISP_RELEASE"
-		buildoptions "/MD"
-		optimize "On"
+		-- Window Build Settings --
 		
-	filter "configurations:Distribution"
-		defines "CRISP_DISTRIBUTION"
-		buildoptions "/MD"
-		optimize "On"
+		filter "system:windows"
+			cppdialect "C++20"
+			systemversion "latest"
+			
+			defines {
+				"CRISP_PLATFORM_WINDOWS"
+			}
+			
+		-- Configuration Settings --
+			
+		filter "configurations:Debug"
+			defines "CRISP_DEBUG"
+			runtime "Debug"
+			symbols "On"
+			
+		filter "configurations:Release"
+			defines "CRISP_RELEASE"
+			runtime "Release"
+			optimize "On"
+			
+		filter "configurations:Distribution"
+			defines "CRISP_DISTRIBUTION"
+			runtime "Release"
+			optimize "On"
