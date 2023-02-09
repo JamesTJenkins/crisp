1cdad502d6ab1646df2df1c08c1f9a6c7bfc8e8e
UndeadKudos <jenkins01tj@gmail.com>
2023-02-09 02:48:38 +0000

    Changed from pointer to ref

--- Crisp/src/Crisp/Renderer/Buffer.cpp
+++ Crisp/src/Crisp/Renderer/Buffer.cpp
@@ -5,26 +5,26 @@
 #include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace Crisp {
-	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size) {
+	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size) {
 		switch (Renderer::GetAPI()) {
 		case RendererAPI::API::None:
 			CRISP_CORE_ASSERT(false, "RendererAPI::None isnt supported.");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return new OpenGLVertexBuffer(vertices, size);
+			return std::make_shared<OpenGLVertexBuffer>(vertices, size);
 		}
 
 		CRISP_CORE_ASSERT(false, "Unknown renderer API.");
 		return nullptr;
 	}
 
-	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size) {
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size) {
 		switch (Renderer::GetAPI()) {
 		case RendererAPI::API::None:
 			CRISP_CORE_ASSERT(false, "RendererAPI::None isnt supported.");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return new OpenGLIndexBuffer(indices, size);
+			return std::make_shared<OpenGLIndexBuffer>(indices, size);
 		}
 
 		CRISP_CORE_ASSERT(false, "Unknown renderer API.");
--- Crisp/src/Crisp/Renderer/VertexArray.cpp
+++ Crisp/src/Crisp/Renderer/VertexArray.cpp
@@ -5,13 +5,13 @@
 #include "Platform/OpenGL/OpenGLVertexArray.h"
 
 namespace Crisp {
-	VertexArray* VertexArray::Create() {
+	Ref<VertexArray> VertexArray::Create() {
 		switch (Renderer::GetAPI()) {
 		case RendererAPI::API::None:
 			CRISP_CORE_ASSERT(false, "RendererAPI::None isnt supported.");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return new OpenGLVertexArray();
+			return std::make_shared<OpenGLVertexArray>();
 		}
 
 		CRISP_CORE_ASSERT(false, "Unknown renderer API.");
